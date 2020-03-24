ad65593123fd6db019dad80bba1259afafda0e97
h4rd1ideveloper <36201098+h4rd1ideveloper@users.noreply.github.com>
2020-03-24 17:33:36 -0300

    upload build files

---
+++ precache-manifest.060de7bea7fd3dd7b7034a89499f20fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9dc4fa7a78d0bdea62cbb379d97a6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fcb7dbf2f8c83405ad6",
+    "url": "/static/css/main.0344f7c8.chunk.css"
+  },
+  {
+    "revision": "886a429acdcf6fd1ad0a",
+    "url": "/static/js/2.a0e693f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a0e693f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fcb7dbf2f8c83405ad6",
+    "url": "/static/js/main.67452a01.chunk.js"
+  },
+  {
+    "revision": "7aff21087884eee70168",
+    "url": "/static/js/runtime-main.7efaf76e.js"
+  }
+]);
